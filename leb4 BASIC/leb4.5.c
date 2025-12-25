a8c7ba18a07d05d52fd3ab0dff0d164695a385e4
witwisit jitwilai <tjitwilai@gmail.com>
2025-12-25 10:43:47 +0700

    update

---
+++ leb4 BASIC/leb4.5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define SIZE 6
+
+int main() {
+    int data[SIZE];
+    int total_sum = 0;
+    int i;
+
+    for (i = 0; i < SIZE; i++) {
+        if (scanf("%d", &data[i]) != 1) {
+            return 1;
+        }
+        total_sum += data[i];
+    }
+
+    printf("\n--- SUMMATION REPORT ---\n");
+    printf("Recorded Numbers: ");
+    for (i = 0; i < SIZE; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    printf("Total Sum of Numbers: %d\n", total_sum);
+    
+    return 0;
+}
